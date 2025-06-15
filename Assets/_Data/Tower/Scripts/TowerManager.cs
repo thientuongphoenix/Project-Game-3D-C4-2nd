bfd7ad7fc932c0dea28cf88b5d80486ad78bfc69
thientuongphoenix <95729186+thientuongphoenix@users.noreply.github.com>
2025-06-15 18:44:51 +0700

    Spawn tower in crosshair position

--- Assets/_Data/Tower/Scripts/TowerManager.cs
+++ Assets/_Data/Tower/Scripts/TowerManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class TowerManager : SaiSingleton<TowerManager>
 {
     [SerializeField] protected TowerCode newTowerId = TowerCode.NoTower;
+    [SerializeField] protected TowerCtrl towerPrefab;
 
     protected virtual void LateUpdate()
     {
@@ -15,7 +16,25 @@ public class TowerManager : SaiSingleton<TowerManager>
 
       if(this.newTowerId == TowerCode.NoTower) return;
 
+      if(this.towerPrefab == null) 
+      {
+        this.towerPrefab = this.GetTowerPrefab(this.newTowerId);
+        if(this.towerPrefab == null) return;
+
+        // Vector3 prefabPos = PlayerCtrl.Instance.transform.position;
+        // prefabPos.x += 2f;
+        // this.towerPrefab.transform.position = prefabPos;
+        this.towerPrefab.SetActive(true);
+      }
+
+      this.towerPrefab.transform.position = PlayerCtrl.Instance.CrosshairPointer.transform.position;
+
       Debug.Log("Placing tower: " + this.newTowerId.ToString());
+    }
+
+    protected virtual TowerCtrl GetTowerPrefab(TowerCode towerCode)
+    {
+      return TowerSpawnerCtrl.Instance.Prefabs.GetByName(towerCode.ToString());
     }
 
     protected virtual TowerCode MapKeyCodeToTowerCode(KeyCode keyCode)
