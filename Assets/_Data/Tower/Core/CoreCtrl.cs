173b74e9ead4303c3fbb1868a62dea08d032ac74
thientuongphoenix <95729186+thientuongphoenix@users.noreply.github.com>
2025-07-16 20:23:30 +0700

    Bring logic game to Hai_Map

--- Assets/_Data/Tower/Core/CoreCtrl.cs
+++ Assets/_Data/Tower/Core/CoreCtrl.cs
@@ -9,15 +9,7 @@ public class CoreCtrl : TowerCtrl
 
     protected override void SetActiveEnemyTargetable()
     {
-        if (this.transform.parent == null)
-        {
-            if (this.enemyTargetable != null) this.enemyTargetable.gameObject.SetActive(true);
-            if (this.towerDamageReceiver != null) this.towerDamageReceiver.gameObject.SetActive(true);
-        }
-        else
-        {
-            if (this.enemyTargetable != null) this.enemyTargetable.gameObject.SetActive(false);
-            if (this.towerDamageReceiver != null) this.towerDamageReceiver.gameObject.SetActive(false);
-        }
+        if (this.enemyTargetable != null) this.enemyTargetable.gameObject.SetActive(true);
+        if (this.towerDamageReceiver != null) this.towerDamageReceiver.gameObject.SetActive(true);
     }
 } 
