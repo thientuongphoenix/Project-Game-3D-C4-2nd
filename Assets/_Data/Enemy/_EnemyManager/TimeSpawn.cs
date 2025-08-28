68e6bca9f36b98a61fe93d3cb6415814e56f602f
phongtt03 <153700652+phongtt03@users.noreply.github.com>
2025-08-28 19:43:25 +0700

    timespawn

---
+++ Assets/_Data/Enemy/_EnemyManager/TimeSpawn.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class TimeSpawn : MonoBehaviour
+{
+    [Header("Objects quản lý")]
+    public GameObject objectA;
+    public GameObject objectB;
+    public GameObject objectC;
+    public GameObject objectD;
+
+    [Header("Thời gian kích hoạt")]
+    public float timeA;
+    public float timeB;
+    public float timeC; 
+    public float timeD;
+
+    private float currentTime = 0f;
+
+    void Start()
+    {
+        // Ban đầu tắt hết
+        SetActiveObjects(false, false, false, false);
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if (currentTime < timeA) 
+        {
+            SetActiveObjects(false, false, false, false);
+        }
+        else if (currentTime < timeB) 
+        {
+            SetActiveObjects(true, false, false, false);
+        }
+        else if (currentTime < timeC) 
+        {
+            SetActiveObjects(false, true, false, false);
+        }
+        else if (currentTime < timeD) 
+        {
+            SetActiveObjects(false, false, true, false);
+        }
+        else // Sau 7 phút
+        {
+            SetActiveObjects(false, false, true, true);
+        }
+    }
+
+    void SetActiveObjects(bool a, bool b, bool c, bool d)
+    {
+        objectA.SetActive(a);
+        objectB.SetActive(b);
+        objectC.SetActive(c);
+        objectD.SetActive(d);
+    }
+}
