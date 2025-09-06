d501d7b8b1bb3f6324fbfedfc5a7705e36507f29
phongtt03 <153700652+phongtt03@users.noreply.github.com>
2025-09-07 03:15:59 +0700

    4 wave

---
+++ Assets/_Data/Effect/Fly/Projectile4DamageSender.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Projectile4DamageSender : EffectDamageSender
+{
+    protected override string GetHitName()
+    {
+        return "Hit1";
+    }
+
+    protected override void Send(DamageReceiver damageReceiver, Collider collider)
+    {
+        if (damageReceiver is PlayerDamageReceiver || damageReceiver is TowerDamageReceiver)
+        {
+            this.effectCtrl.Despawn.DoDespawn();
+            return;
+        }
+
+        base.Send(damageReceiver, collider);
+    }
+}
